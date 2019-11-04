a9094498f3e44ad4e5995e9d108c81e1c4d3f325
asweenravi <asweenravi@gmail.com>
2019-11-04 14:18:25 +0530

    static link of the pages

--- src/app/pages/story-listing/story-listing.page.ts
+++ src/app/pages/story-listing/story-listing.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-story-listing',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoryListingPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  goToStoryPointing() {
+    this.router.navigate(['story-point']);
+  }
 }
